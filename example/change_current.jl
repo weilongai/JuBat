659bd3b2bc020851b86ad89fceb22f4413cbbda6
Weilong Ai <weilong.ai@outlook.com>
2023-04-05 17:20:19 +0800

    add examples for paper

--- example/change_current.jl
+++ example/change_current.jl
@@ -1,22 +1,26 @@
 using Plots, CSV, DataFrames, Interpolations
 include("../src/JuBat.jl") 
-
 param_dim = JuBat.ChooseCell("LG M50")
 opt = JuBat.Option()
-path = pwd()
-file= path * "/src/data/drive_cycles/US06.csv"
-data = CSV.read(file, DataFrame, header = 2)
+path = pwd() * "/src/data/drive_cycles"
+data = CSV.read(path * "/UDDS.csv", DataFrame, header = 3)
 current=Matrix(data)
 current_intpolation = LinearInterpolation(current[:,1],current[:,2])
 opt.Current = x-> current_intpolation(x)
-opt.time = collect(0:1:600)
-opt.dt = [0.1, 12]
+opt.time = collect(0:1:1369)
+opt.dt = [0.5, 12]
 opt.model = "P2D"
 case1 = JuBat.SetCase(param_dim, opt)
 result = JuBat.Solve(case1)
-
-# Plot
 times = result["time [s]"]
 voltage = result["cell voltage [V]"]
 current = result["cell current [A]"]
-plot(times, voltage, label="P2D",xlabel="time [s]", ylabel="cell voltage [V]", lw=1)
+plot(times, voltage, label="JuBat",xlabel="time [s]", ylabel="cell voltage [V]",linecolor=:black, lw=1.5, fontsize=12, size=(400,300))
+path = pwd() * "/src/data/"
+result = CSV.read(path * "pybamm_drive_P2D_UDDS.csv", DataFrame, header = 1)
+pybamm_drive = Matrix(result)
+plot!(pybamm_drive[:,1], pybamm_drive[:,3],label="PyBaMM", linestyle=:dot, linecolor=:red, lw=2, title="Drive cycle UDDS", legend=(0.8,0.95))
+savefig("drive_UDDS_V.pdf")
+
+# plot(times, current,xlabel="time [s]", ylabel="current [A]", label="Current", fontsize=12, size=(400,300), title="Drive cycle UDDS")
+# savefig("drive_UDDS_I.pdf")
