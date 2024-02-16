2bf38e97ecaf00137a33b25dcaf94e53f8edc997
Weilong Ai <weilong.ai@outlook.com>
2024-02-16 13:26:26 +0800

    corrent legend position

--- example/thermal_example.jl
+++ example/thermal_example.jl
@@ -1,34 +1,35 @@
 using Plots, CSV, DataFrames
 include("../src/JuBat.jl") 
-param_dim = JuBat.ChooseCell("Northrop")
+param_dim = JuBat.ChooseCell("LG M50")
 param_dim.cell.v_l = 2.5
 opt = JuBat.Option()
 opt.thermalmodel = "lumped"
 colors = [:black, :blue, :red, :green]
 path = pwd() * "/src/data/"
-opt.dtType = "constant"
+opt.dtType = "auto"
 opt.jacobi = "update"
-Crates = [1, 3] #Crates = [1, 3, 5]
-I1C = 30 * 2.05272
-pV = plot(xlabel="Output capacity [Ah]", ylabel="Cell voltage [V]", legend_column = 2)
-pT = plot(xlabel="Output capacity [Ah]", ylabel="Temperature [K]", legend_column = 2)
+Crates = 1
+I1C = 5
+pV = plot(xlabel="Output capacity [Ah]", ylabel="Cell voltage [V]")
+ylims!(2.5, 4.3)
+pT = plot(xlabel="Output capacity [Ah]", ylabel="Temperature [K]", legend=:bottomright)
 for i in eachindex(Crates)
     opt.Current = x->  I1C * Crates[i]
-    if i < 3
-        opt.dt = [0.1, 0.1]
+    if Crates[i] < 2
+        opt.dt = [1 10] / Crates[i]
     else
-        opt.dt = [0.01, 0.01]
+        opt.dt = [1 10] / Crates[i] / 4
     end
-    opt.time = [0 3540/Crates[i]]
-    opt.model = "SPMe" # choose model, other options are "SPM" or "SPMe"
+    opt.time = [0 3600/Crates[i]]
+    opt.model = "P2D" # choose model, other options are "SPM" or "SPMe"
     case1 = JuBat.SetCase(param_dim, opt)
     result = JuBat.Solve(case1)
-    plot!(pV, result["time [s]"]/3600*Crates[i]*I1C, result["cell voltage [V]"], label=string(Crates[i]) * "C (JuBat)", linecolor=colors[i], legend_column = 2)
-    plot!(pT, result["time [s]"]/3600*Crates[i]*I1C, result["temperature [K]"], label=string(Crates[i]) * "C (JuBat)", linecolor=colors[i], legend_column = 2)
-    result1 = CSV.read(path * "lionsimba" * string(Crates[i]) * "c.csv", DataFrame, header = 1)
-    lionsimba = Matrix(result1)
-    plot!(pV, lionsimba[:,1]/3600*Crates[i]*I1C, lionsimba[:,3],label=string(Crates[i]) * "C (LIONSIMBA)", linestyle =:dot, linecolor=colors[i], lw=2, legend_column = 2)
-    plot!(pT, lionsimba[:,1]/3600*Crates[i]*I1C, lionsimba[:,4],label=string(Crates[i]) * "C (LIONSIMBA)", linestyle =:dot, linecolor=colors[i], lw=2, legend_column = 2)
+    plot!(pV, result["time [s]"]/3600*Crates[i]*5, result["cell voltage [V]"], label=string(Crates[i]) * "C (JuBat)", linecolor=colors[i])
+    plot!(pT, result["time [s]"]/3600*Crates[i]*5, result["temperature [K]"], label=string(Crates[i]) * "C (JuBat)", linecolor=colors[i])
+    result1 = CSV.read(path * "pybamm_P2D_" * string(Crates[i]) * "C_T.csv", DataFrame, header = 1)
+    result1 = Matrix(result1)
+    plot!(pV, result1[:,1]/3600*Crates[i]*5, result1[:,3],label=string(Crates[i]) * "C (PyBaMM)", linestyle =:dot, linecolor=colors[i], lw=2)
+    plot!(pT, result1[:,1]/3600*Crates[i]*5, result1[:,4],label=string(Crates[i]) * "C (PyBaMM)", linestyle =:dot, linecolor=colors[i], lw=2)
 end
 
 savefig(pV, "thermal example-V.pdf")
